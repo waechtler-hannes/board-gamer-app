814f7d965a4e5e5e51efab607483105784221c2c
Hannes Wächtler <waechtler.hannes@gmail.com>
2025-04-10 11:11:20 +0200

    Test  2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hallo Welt!</Text>
+      <Text>Hallo Welt!hshaihbduihadh</Text>
       <StatusBar style="auto" />
     </View>
   );
