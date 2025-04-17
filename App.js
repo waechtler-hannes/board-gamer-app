e8d7e39a6fbb25b0196fbe8ade6465a9a6460319
Regina Spangler <regina.spangler@iu-study.org>
2025-04-17 12:41:38 +0200

    Hallo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hallo Welt!hshaihbduihadh</Text>
+      <Text>Hallo Welt!hshaihbd</Text>
       <StatusBar style="auto" />
     </View>
   );
