aacdc61fefc5c1599d62461c2703cb8e57320f59
Hannes Wächtler <waechtler.hannes@gmail.com>
2025-04-23 12:03:17 +0200

    install Firebase

---
+++ firebaseConfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// Optionally import the services that you want to use
+// import {...} from 'firebase/auth';
+// import {...} from 'firebase/database';
+// import {...} from 'firebase/firestore';
+// import {...} from 'firebase/functions';
+// import {...} from 'firebase/storage';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDnrUarr-MIJE_gGMucOWK_MoL-TgEeTV8",
+  authDomain: "teamplay-717ad.firebaseapp.com",
+  projectId: "teamplay-717ad",
+  storageBucket: "teamplay-717ad.firebasestorage.app",
+  messagingSenderId: "673267362378",
+  appId: "1:673267362378:web:48d97bf5c4e5c8881794b3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
