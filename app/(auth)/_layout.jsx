07363b47c3b6ed7566d6b1bc4cbf2070c682a4fe
Hannes Wächtler <waechtler.hannes@gmail.com>
2025-05-08 15:10:39 +0200

    Tutorial 15 abgeschlossen

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { StatusBar } from 'react-native'
 import { Stack } from 'expo-router'
+import { useUser } from '../../hooks/useUser'
 
 export default function UserLayout() {
+
+  const { user } = useUser()
+  console.log(user)
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
